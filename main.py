05ca7ed3479413b8fc4d6c2b07a38939375aee4d
Syntax-Programmer <155230834+Syntax-Programmer@users.noreply.github.com>
2024-10-18 21:41:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import mysql.connector as sqltor
 
+
 def create_con():
-    con = sqltor.connect(user="anand_maurya", passwd="ANAND6308anand", database="SchoolProject", host="localhost")
+    con = sqltor.connect(
+        user="anand_maurya",
+        passwd="ANAND6308anand",
+        database="SchoolProject",
+        host="localhost",
+    )
     if not con.is_connected():
         print("Can't connect to the database")
     cursor = con.cursor()
